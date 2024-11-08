c93d90b0f50f3091643d68255878779c99971d67
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2024-11-08 19:59:11 +0530

    modified

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let celcius=document.getElementById('celcuis');
+
+let celsius=document.getElementById('celsius');
 let fahrenheit=document.getElementById('fahrenheit');
-let kelvin=document.getElementById('kevin');
-function submit(){
-    
+let kelvin=document.getElementById('kelvin');
+
+celsius.oninput =()=>{
+    let CelsiusFahrenheit=(parseFloat(celsius.value)*9)/5+32;
+    fahrenheit.value=parseFloat(CelsiusFahrenheit.toFixed(3));
+    let CelsiusKelvin=parseFloat(celsius.value)+273.15;
+    kelvin.value=parseFloat(CelsiusKelvin.toFixed(3));
+};
+
+fahrenheit.oninput=()=>{
+    let FahrenheitCelsius=(parseFloat(fahrenheit.value)-32)*5/9;
+    celsius.value=parseFloat(FahrenheitCelsius.toFixed(3));
+    let FahrenheitKelvin=(parseFloat(fahrenheit.value)-32)*5/9+273.15;
+    kelvin.value=parseFloat(FahrenheitKelvin.toFixed(3))
+}
+
+kelvin.oninput=()=>{
+    let KelvinCelsius=parseFloat(kelvin.value)-273.15;
+    celsius.value=parseFloat(KelvinCelsius.toFixed(3));
+    let KelvinFahrenheit=parseFloat((kelvin.value)-273.15)* 9/5+32;
+    fahrenheit.value=parseFloat(KelvinFahrenheit.toFixed(3));
 }
